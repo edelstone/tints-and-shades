829a2784457e06e4b3c0b445dbbc3079346bf6b0
Michael Edelstone <michael.edelstone@gmail.com>
2021-08-17 12:31:20 -0500

    provides a couple more opportunities for Carbon ads to recycle

--- scripts/tints-and-shades.js
+++ scripts/tints-and-shades.js
@@ -234,7 +234,7 @@ $(document).keypress(function(event){
 });
 
 // Show a new Carbon ad every time the submit button is clicked
-$('#loadmore').on('click', function() {
+$('#loadmore, #tints-and-shades, .switch, .textarea-label').on('click', function() {
   // If the ad hasn't loaded yet, don't refresh it while it's still loading, or it will return two (or more) ads
   if (!$("#carbonads")[0]) return;
   // If the script hasn't loaded, don't try calling it
